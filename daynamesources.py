a1bfc928a516da6c9c6e8d1ffac85cb4dc2da215
Alex <awintschel@gmail.com>
2018-09-11 22:04:59 -0700

    completed, tested and ready for cronjob

--- daynamesources.py
+++ daynamesources.py
@@ -5,11 +5,9 @@ app_key = '0dd1b3c11be7595c17e5dba593dbf36c'
 # api_url_base = 'https://od-api.oxforddictionaries.com/api/v1'
 
 wordlist_url = 'https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/domains=Art'
-
 day_request = requests.get(wordlist_url, headers = {'app_id': app_id, 'app_key': app_key})
 words = day_request.json()
 
-words['results'][0]['word']
 
 def cleanhtml(raw_html):
   cleanr = re.compile('<.*?>|\n')
@@ -17,10 +15,12 @@ def cleanhtml(raw_html):
   return cleantext
 
 def day():
-    new_day = 'test'
+    new_day = ''
+    for i in range(3):
+        new_day += " " + words['results'][random.randint(0,99)]['word']
     return new_day
 
 def because():
     get_quote = requests.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&').json()
-    new_reason = cleanhtml(new_reason[0]['content'])
+    new_reason = cleanhtml(get_quote[0]['content'])
     return new_reason
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import requests, daynamesources
+import requests
+from daynamesources import day, because
 
-state_identity = 'Plowman'
-
-url = 'https://docs.google.com/forms/d/e/1FAIpQLScC1fY4nkNKuX8co7ivxEoT0XGX4Yz_T5UIrTruvOk7Aqa8MA/formResponse'
-
-payload = {'entry.895362114': state_identity,
-           'entry.346635073': day(),
-           'entry.49460226': because(),
+state_identity = 'Plowface'
+url = 'https://docs.google.com/forms/d/e/1FAIpQLSdgCgjG50gWnO5DJbsyibTqNEKCcUeFRBCQbCKEpHp46mjVSw/formresponse'
+payload = {'entry.522972278': state_identity,
+           'entry.917801070': day(),
+           'entry.1000005': because(),
            'draftResponse':[],
            'pageHistory':0}
-
-user_agent = {'Referer':'https://docs.google.com/forms/d/e/1FAIpQLScC1fY4nkNKuX8co7ivxEoT0XGX4Yz_T5UIrTruvOk7Aqa8MA/viewform','User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36'}
-
+user_agent = {'Referer':'https://docs.google.com/forms/d/e/1FAIpQLSdgCgjG50gWnO5DJbsyibTqNEKCcUeFRBCQbCKEpHp46mjVSw/viewform','User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36'}
 r = requests.post(url, data=payload, headers=user_agent)
